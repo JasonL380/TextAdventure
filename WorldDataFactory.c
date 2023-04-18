f0a9c64f7f92f3ab7c572671ab58bb43afdb6907
elementalearth123 <72054738+elementalearth123@users.noreply.github.com>
2023-04-18 14:10:28 -0700

    Added path 1

--- WorldDataFactory.c
+++ WorldDataFactory.c
@@ -17,6 +17,7 @@ This could be used to create default states as well as loaded state.
 #include "Source/ItemList.h" /* ItemList_Add */
 #include "Source/BrickFunctions.h" /* Brick_Build */
 #include "Source/GoldPieceFunctions.h" /* GoldPiece_Build */
+#include "Source/CircleKeyFunctions.h" /* Circle_Key_Build */
 #include "Source/ExitDoorFunctions.h" /* ExitDoor_Build */
 
 
@@ -52,10 +53,12 @@ Room* Room_Start_Build()
 	/* Pre-declare a room pointer which we will use to build the new room */
 	Room* room = NULL;
 
-	room = Room_Create("You have been trapped in a dungeon of riddles. There are three paths ahead of you, each with their own key. However, you must figure out the rule to proceed in each room.\n");
+	room = Room_Create("You have been trapped in a dungeon of riddles. There are three paths ahead of you, each with their own key. However, you must figure out the rule to proceed in each room. Return here once you've obtained the keys and use them.\n");
 
-    Room_AddRoomExit(room, "north", 1);
-    Room_AddRoomExitShortcut(room, "n", 1);
+	Room_AddRoomExit(room, "s", 12);
+
+    Room_AddRoomExit(room, "southwest", 1);
+    Room_AddRoomExitShortcut(room, "sw", 1);
 
     ItemList_AddItem(Room_GetItemList(room), ExitDoor_Build());
 
@@ -69,17 +72,30 @@ Room* Path_1_1_Build()
 	/* Pre-declare a room pointer which we will use to build the new room */
 	Room* room = NULL;
 
-	room = Room_Create("This is room 1.  There is a large mirror here, and it shimmers as you approach.\n");
+	room = Room_Create("This room has 2 pillars on one side and 2 on the other and a locked door in front of you. Perhaps the pillars hold the key?\n");
 
-    Room_AddRoomExit(room, "through the mirror", 2);
-    Room_AddRoomExitShortcut(room, "through mirror", 2);
-    Room_AddRoomExitShortcut(room, "mirror", 2);
+    Room_AddRoomExitShortcut(room, "4", 10);
+    Room_AddRoomExitShortcut(room, "four", 10);
 
-    Room_AddRoomExit(room, "south", 0);
+    Room_AddRoomExit(room, "north", 0);
+    Room_AddRoomExitShortcut(room, "n", 0);
 
-    Room_AddRoomExitShortcut(room, "s", 0);
+	/* return the new room */
+	return room;
+}
 
-    ItemList_AddItem(Room_GetItemList(room), Brick_Build());
+Room* Path_11_mid_Build()
+{
+	/* Pre-declare a room pointer which we will use to build the new room */
+	Room* room = NULL;
+
+	room = Room_Create("The door opens in front of you.\n");
+
+	Room_AddRoomExit(room, "south", 2);
+	Room_AddRoomExitShortcut(room, "s", 2);
+
+	Room_AddRoomExit(room, "north", 0);
+	Room_AddRoomExitShortcut(room, "n", 0);
 
 	/* return the new room */
 	return room;
@@ -90,17 +106,32 @@ Room* Path_1_2_Build()
 	/* Pre-declare a room pointer which we will use to build the new room */
 	Room* room = NULL;
 
-	room = Room_Create("This is room 1.  There is a large mirror here, and it shimmers as you approach.\n");
+	room = Room_Create("This next room has 4 pillars on one side and 3 on the other, and once again a locked door in front.\n");
 
-	Room_AddRoomExit(room, "through the mirror", 2);
-	Room_AddRoomExitShortcut(room, "through mirror", 2);
-	Room_AddRoomExitShortcut(room, "mirror", 2);
+	Room_AddRoomExitShortcut(room, "12", 11);
+	Room_AddRoomExitShortcut(room, "twelve", 11);
 
-	Room_AddRoomExit(room, "south", 0);
-
-	Room_AddRoomExitShortcut(room, "s", 0);
+	Room_AddRoomExit(room, "north", 1);
+	Room_AddRoomExitShortcut(room, "n", 1);
 
 	ItemList_AddItem(Room_GetItemList(room), Brick_Build());
+
+	/* return the new room */
+	return room;
+}
+
+Room* Path_12_mid_Build()
+{
+	/* Pre-declare a room pointer which we will use to build the new room */
+	Room* room = NULL;
+
+	room = Room_Create("The door opens in front of you.\n");
+
+	Room_AddRoomExit(room, "south", 3);
+	Room_AddRoomExitShortcut(room, "s", 3);
+
+	Room_AddRoomExit(room, "north", 1);
+	Room_AddRoomExitShortcut(room, "n", 1);
 
 	/* return the new room */
 	return room;
@@ -111,22 +142,33 @@ Room* Path_1_3_Build()
 	/* Pre-declare a room pointer which we will use to build the new room */
 	Room* room = NULL;
 
-	room = Room_Create("This is room 1.  There is a large mirror here, and it shimmers as you approach.\n");
+	room = Room_Create("This final room has 6 pillars on one side, 7 on the other, and a circular gem locked in a cage, one of the three keys.\n");
 
-	Room_AddRoomExit(room, "through the mirror", 2);
-	Room_AddRoomExitShortcut(room, "through mirror", 2);
-	Room_AddRoomExitShortcut(room, "mirror", 2);
+	Room_AddRoomExitShortcut(room, "42", 12);
+	Room_AddRoomExitShortcut(room, "forty two", 12);
 
-	Room_AddRoomExit(room, "south", 0);
-
-	Room_AddRoomExitShortcut(room, "s", 0);
-
-	ItemList_AddItem(Room_GetItemList(room), Brick_Build());
+	Room_AddRoomExit(room, "north", 2);
+	Room_AddRoomExitShortcut(room, "n", 2);
 
 	/* return the new room */
 	return room;
 }
 
+Room* Path_13_mid_Build()
+{
+	/* Pre-declare a room pointer which we will use to build the new room */
+	Room* room = NULL;
+
+	room = Room_Create("The cage opens in front of you, revealing the key.\n");
+
+	Room_AddRoomExit(room, "north", 2);
+	Room_AddRoomExitShortcut(room, "n", 2);
+
+	ItemList_AddItem(Room_GetItemList(room), Circle_Key_Build());
+
+	/* return the new room */
+	return room;
+}
 
 Room* Path_2_1_Build()
 {
@@ -234,7 +276,7 @@ WorldData* CreateInitialWorldData()
 
 	/* TODO REQUIRED: update room count to match the number of rooms you have created and added to the world
 	   if this number doesn't match then your game will either crash or you will end up stuck in a broken room with no exits */
-	int roomCount = 4;
+	int roomCount = 13;
 
 	/* create the new WorldData object with 3 rooms */
 	worldData = WorldData_Create("Welcome to my GAM100 Game!\n\n", roomCount);
@@ -245,6 +287,10 @@ WorldData* CreateInitialWorldData()
     WorldData_SetRoom(worldData, 1, Path_1_1_Build());
 	WorldData_SetRoom(worldData, 2, Path_1_2_Build());
 	WorldData_SetRoom(worldData, 3, Path_1_3_Build());
+
+	WorldData_SetRoom(worldData, 10, Path_11_mid_Build());
+	WorldData_SetRoom(worldData, 11, Path_12_mid_Build());
+	WorldData_SetRoom(worldData, 12, Path_13_mid_Build());
 
 	WorldData_SetRoom(worldData, 4, Path_2_1_Build());
 	WorldData_SetRoom(worldData, 5, Path_2_2_Build());
